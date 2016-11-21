a4bade6f4415632ea23c8102dd137380163f4f2a
karice <kl529@naver.com>
2016-11-21 18:02:13 +0900

    connect tvDate to layout

--- MainActivity.java
+++ MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(android.R.layout.activity_main);
+        setContentView(android.activity_main);
 
         tvDate = (TextView)findViewById(android.R.id.tv_date);
 
