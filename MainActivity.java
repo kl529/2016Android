2a5cc0d89a6a72384e8bbc1bfe5f135551b941fb
kimhyebeen <kimhbin@naver.com>
2016-11-24 16:28:57 +0900

    Update MainActivity.java
    I Add days.

--- MainActivity.java
+++ MainActivity.java
@@ -42,17 +42,27 @@ public class MainActivity extends Activity {
         setContentView(android.R.layout.activity_main);
 
         tvDate = (TextView)findViewById(android.R.id.tv_date);
-
+        gridView = (GridView)findViewById(R.id.gridview);
+        //Set today's month and days.
         long now = System.currentTimeMillis();
         
         final Date date = new Date(now);
-
+        //Each save year,month,days
         final SimpleDateFormat nowYear = new SimpleDateFormat("yyyy",Locale.KOREA);
         final SimpleDateFormat nowMonth = new SimpleDateFormat("MM",Locale.KOREA);
         final SimpleDateFormat nowDay = new SimpleDateFormat("DD",Locale.KOREA);
         
         tvDate.setText(nowYear.format(date) + "." + nowMonth.format(date));
-        
+        //gridview what day?
+        dayList = new ArrayList<String>();
+        dayList.add("SUN");
+        dayList.add("MON");
+        dayList.add("TUE");
+        dayList.add("WED");
+        dayList.add("THU");
+        dayList.add("FRI");
+        dayList.add("SAT");
+        mCal = Calendar.getInstance();
     }
 
 }
