97b23da8400eb988606f9c10d865eea11e06d642
kimhyebeen <kimhbin@naver.com>
2016-11-29 15:29:58 +0900

    I fixed wrong word_2

--- MainActivity.java
+++ MainActivity.java
@@ -148,7 +148,7 @@ public class MainActivity extends Activity {
             public View getView(int position, View convertView, ViewGroup parent){//getview
                 ViewHolder holder = null; //create ViewHolder
                 if (convertView == null){
-                    convertView = inflater.inflate(R.layout.item_calender_gridview, parent, false);
+                    convertView = inflater.inflate(R.layout.item_calendar_gridview, parent, false);
                 }
             }
         private class ViewHolder{
