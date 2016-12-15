400e9eefa2c3c880b811dbe11e9970d3756de432
kimhyebeen <kimhbin@naver.com>
2016-12-15 23:02:40 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 import android.app.Activity;
 import android.content.Context;
-    import android.os.Bundle;
+import android.os.Bundle;
     import android.view.LayoutInflater;
     import android.view.View;
     import android.view.View.OnClickListener;
