a8372f50dea43e13088a6d32cc4f4592bc00322b
kimhyebeen <kimhbin@naver.com>
2016-12-15 23:00:42 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
     import java.util.ArrayList;
     import java.util.Calendar;
     import java.util.Date;
-    import java.util.List;
     import android.app.Activity;
     import android.content.Context;
     import android.os.Bundle;
