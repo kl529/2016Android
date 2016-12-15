ba105494fa79025e42d9a30488d3e5ed4f26dc18
kimhyebeen <kimhbin@naver.com>
2016-12-15 23:02:20 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,7 +2,7 @@ package com.example.kl529.calendar;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
-    import android.app.Activity;
+import android.app.Activity;
     import android.content.Context;
     import android.os.Bundle;
     import android.view.LayoutInflater;
