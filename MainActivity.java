58a513b6a6ff4a368203fca761723239151b3ed9
kimhyebeen <kimhbin@naver.com>
2016-12-16 14:02:19 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-    import android.view.ViewGroup;
+import android.view.ViewGroup;
     import android.widget.BaseAdapter;
     import android.widget.GridView;
     import android.widget.TextView;
