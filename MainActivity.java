68e1af1a9ccda4bb75bd161fbcb2dba477bf65eb
kimhyebeen <kimhbin@naver.com>
2016-12-16 14:08:48 +0900

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-    import android.widget.GridView;
+import android.widget.GridView;
     import android.widget.TextView;
 
 public class MainActivity extends Activity {
