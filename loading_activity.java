ff0f070a969a05be2c5b3e3ace0b252daadfbaf0
karice <kl529@naver.com>
2016-12-19 23:38:53 +0900

    get default oncreate

--- loading_activity.java
+++ loading_activity.java
@@ -7,6 +7,9 @@ public class LoadingActivity extends AppCompatActivity {
   
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
 
     }
 }
