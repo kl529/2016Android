e6e6a7206353180f5f6b5b0914218a84c979c5bf
karice <kl529@naver.com>
2016-12-19 23:39:12 +0900

    add handler to set time

--- loading_activity.java
+++ loading_activity.java
@@ -9,6 +9,8 @@ public class LoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+      
+        Handler hd = new Handler(){
 
 
     }
