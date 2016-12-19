4fa43ba5e9f38e0d53476bf02a6f6a1cb3e1fd8c
karice <kl529@naver.com>
2016-12-19 23:35:48 +0900

    add loading image program

---
+++ LoadingActivity.java
@@ -0,0 +1,22 @@
+package com.example.kl529.calendar;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Message;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        Handler hd = new Handler(){
+          public void handleMessage(Message msg){
+              finish();
+          }
+        };
+        hd.sendEmptyMessageDelayed(0,3000);
+    }
+}
